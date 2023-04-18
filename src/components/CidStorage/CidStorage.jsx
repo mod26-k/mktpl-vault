bc0974e224b4e07f495073917ad585fdfb0bfdff
mod26-k <kellirinaw@gmail.com>
2023-04-18 06:22:20 -1000

    finalized cidstorage css

--- src/components/CidStorage/CidStorage.jsx
+++ src/components/CidStorage/CidStorage.jsx
@@ -3,19 +3,33 @@ import './CidStorage.css'
 
 export default function CidStorage() {
   return (
-    <div>
+    <div className='container'>
         <h1>Cid Storage</h1>
         <table>
-            <tr>
-                <th>title</th>
-                <th>file type</th>
-                <th>cid</th>
-            </tr>
-            <tr>
-                {/* <td>{cid.title}</td>
-                <td>{cid.fileType}</td>
-                <td>{cid.cid}</td> */}
-            </tr>
+            <thead>
+                <tr>
+                    <th>title</th>
+                    <th>file type</th>
+                    <th>cid</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    {/* <td>{cid.title}</td>
+                    <td>{cid.fileType}</td>
+                    <td>{cid.cid}</td> */}
+                </tr>
+                <tr>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                </tr>
+                <tr>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                </tr>
+            </tbody>
         </table>
     </div>
   )
